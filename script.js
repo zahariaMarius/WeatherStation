fab5ff4b4cb2a4c0c9c9d3bfc2139af3816b787a
zaharia laurentiu jr marius <laurentiu.zaharia@edu.itspiemonte.it>
2017-11-30 09:28:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * @Author: Zaharia Laurentiu Jr Marius
+ * @Date:   2017-11-30T09:25:57+01:00
+ * @Email:  laurentiu.zaharia@edu.itspiemonte.it
+ * @Project: WeatherStation
+ * @Filename: script.js
+ * @Last modified by:   Zaharia Laurentiu Jr Marius
+ * @Last modified time: 2017-11-30T09:26:51+01:00
+ */
