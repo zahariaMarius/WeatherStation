596186bf0e3818e420c0bb6173197fb933eb799b
zaharia laurentiu jr marius <laurentiu.zaharia@edu.itspiemonte.it>
2017-12-07 12:23:11 +0100

    body creato al click e data al refresh funzionante

--- js/script.js
+++ js/script.js
@@ -5,12 +5,60 @@
  * @Project: WeatherStation
  * @Filename: script.js
  * @Last modified by:   Zaharia Laurentiu Jr Marius
- * @Last modified time: 2017-12-04T21:16:30+01:00
+ * @Last modified time: 2017-12-07T12:22:43+01:00
  */
+
+
+ // $("#input-station-name").on("keyup", function() {
+ //   var value = $(this).val().toLowerCase();
+ //   $(".stationName").filter(function() {
+	//    //console.log($(this).index(".headerAccordion"));
+	//    //$(".accordion").toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	//  //$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+	//  //console.log($(this).index('.header'));
+	//  if ($(this).text().toLowerCase().indexOf(value) > -1) {
+	//  	console.log("index: " + $(this).index('.headerAccordion'));
+	// 	var a = $(this).index('.headerAccordion');
+	// 	$('.accordion').eq(a).show();
+	// 	//$('.accordion')[a].toggle($(this).text().toLowerCase().indexOf(value) > -1);
+	// }else {
+	// 	var a = $(this).index('.headerAccordion');
+	// 	$('.accordion').eq(a).hide();
+	// }
+	//  //console.log("cazzo");
+	//  //console.log($(this).text().toLowerCase().indexOf(value));
+ // });
+ // });
+ //
+ // $("#select-country").on("change", function() {
+ //   var value = $(this).val().toLowerCase();
+ //   console.log(value);
+ //   $(".headerAccordion").filter(function() {
+	//   //console.log($(this).index(".headerAccordion"));
+	//   //$(".accordion").toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	// //$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+	// //console.log($(this).index('.header'));
+	// if ($(this).text().toLowerCase().indexOf(value) > -1) {
+	//    console.log("index: " + $(this).index('.headerAccordion'));
+	//    var a = $(this).index('.headerAccordion');
+	//    $('.accordion').eq(a).show();
+	//    //$('.accordion')[a].toggle($(this).text().toLowerCase().indexOf(value) > -1);
+ //   }else {
+	//    console.log("nessuno");
+	//    var a = $(this).index('.headerAccordion');
+	//    $('.accordion').eq(a).hide();
+ //   }
+	// //console.log("cazzo");
+	// //console.log($(this).text().toLowerCase().indexOf(value));
+ // });
+ // });
+
+
 
 /**
  * [selectedItem array that contain all opened accordion and it's heigth]
  * @type {Array}
+ *
  */
 var selectedItem = [];
 
@@ -28,8 +76,9 @@ function getApiWeatherData() {
 		console.log("success");
 		console.log(weatherData);
 		$('#accordionContainer').empty();
+		getLastUpdate();
 		createAccordions(weatherData);
-		addEventListenerToAccordion();
+		//addEventListenerToAccordion(weatherData);
 	})
 	.fail(function(error) {
 		console.log(error);
@@ -41,39 +90,7 @@ function getApiWeatherData() {
 		console.log("ajax call complete");
 	});
 
-	var refreshData = setTimeout(getApiWeatherData, 15000);
-}
-
-/**
- * [apllyAccordionAnimation function that aplly animation for all accordion created]
- * @return {[type]} [description]
- */
-function applyAccordionAnimation() {
-	var accordions = $('.headerAccordion');
-
-	for (var i = 0; i < accordions.length; i++) {
-		accordions[i].onclick = function() {
-			this.classList.toggle("active");
-			var bodyAccordion = this.nextElementSibling;
-			if(bodyAccordion.style.maxHeight) {
-				bodyAccordion.style.maxHeight = null;
-			}else {
-				bodyAccordion.style.maxHeight = bodyAccordion.scrollHeight + "px";
-			}
-		}
-	}
-}
-
-/**
- * [addEventListenerToAccordion function that add the event listener to all accordion, and save the body heigth if open]
- */
-function addEventListenerToAccordion() {
-	$('.accordion').click(function (e){
-		var index = $(this).index('.accordion');
-		var height = $('.bodyAccordion')[index].style.maxHeight;
-		selectedItem[index] = height;
-		console.log(selectedItem);
-	});
+	//var refreshData = setTimeout(getApiWeatherData, 15000);
 }
 
 /**
@@ -105,7 +122,7 @@ function createAccordion(singleWeatherData) {
 	 * [accordionDiv that contain the header and body]
 	 * @type {[type]}
 	 */
-	var accordionDiv = $('<div></div>').addClass('accordion')
+	var accordionDiv = $('<div></div>').addClass('accordion');
 
 	/**
 	 * [headerAccordionDiv is the header of accordion]
@@ -116,22 +133,11 @@ function createAccordion(singleWeatherData) {
 	 * [bodyAccordionDiv is the body of accordion]
 	 * @type {[type]}
 	 */
-	var bodyAccordionDiv = createBodyAccordion(singleWeatherData);
 
 	//append the header and body to accordion
-	accordionDiv.append(headerAccordionDiv, bodyAccordionDiv);
+	accordionDiv.append(headerAccordionDiv);
 	//append accordion to container
 	accordionContainer.append(accordionDiv);
-	//save accordionDiv index
-	var accordionDivIndex = accordionDiv.index('.accordion');
-
-	//check if the accordion was open, if true create it open
-	if (checkIfAccordionIsOpen(accordionDivIndex)) {
-		bodyAccordionDiv.css('max-height', selectedItem[accordionDivIndex]);
-	}
-
-	//call function that apply animation to accordion
-	applyAccordionAnimation();
 }
 
 /**
@@ -152,22 +158,72 @@ function craeteHeaderAccordion(singleWeatherData) {
 	 */
 	var nationFlag = getFlagImage(singleWeatherData);
 
-	var stationName = singleWeatherData.station.name;
+
+	var stationName = $('<p>' + singleWeatherData.station.name + '</p>').addClass('stationName');
 
 	var weatherIcon = getWeatherIcon(singleWeatherData);
 
-	var temperature = singleWeatherData.temperature;
+	var temperature = $('<p>' + singleWeatherData.temperature + '</p>');
+	temperature.addClass('temperature');
 	var temperatureMax = singleWeatherData.temperature_max;
 	var temperatureMin = singleWeatherData.temperature_min;
 	var relativeHumidity = singleWeatherData.relative_humidity;
 	var windStrength = singleWeatherData.wind_strength;
 
-	headerAccordionDiv.html("Nome stazione: " + stationName +
-		" Temperatura:" + temperature + " Temperatura massima: " + temperatureMax +
-		" Temperatura minima: " + temperatureMin + " Umidità: " + relativeHumidity+"%" +
-		" Velocità vento: " + windStrength).append(weatherIcon, nationFlag);
+	headerAccordionDiv.html("Max: " + temperatureMax + " Min: " +
+						temperatureMin + ", umidità: " + relativeHumidity
+						+ ", vento: " + windStrength).prepend(nationFlag,
+						stationName).append(weatherIcon, temperature);
 
+	headerAccordionDiv.click(function(event) {
+		if (checkIfBodyAccordionNotExist($(this))) {
+			createBodyAccordionEventListener($(this), singleWeatherData);
+		}
+		applyBodyAccordionAnimation($(this));
+	});
 	return headerAccordionDiv;
+}
+
+/**
+ * [applyBodyAccordionAnimation function that aplly the animation on body]
+ * @param  {[type]} header [header element clicked]
+ * @return {[type]}        [description]
+ */
+function applyBodyAccordionAnimation(header) {
+	var bodyAccordion = header.next('.bodyAccordion');
+	console.log("bodyAccordion");
+	console.log(bodyAccordion.prop("scrollHeight"));
+		if(bodyAccordion[0].style.maxHeight) {
+			bodyAccordion[0].style.maxHeight = null;
+		}else {
+			bodyAccordion[0].style.maxHeight = bodyAccordion[0].scrollHeight + "px";
+		}
+}
+
+/**
+ * [checkIfBodyAccordionExist function that check if the requested body already exist]
+ * @param  {[type]} header [header element clicked]
+ * @return {[type]}        [description]
+ */
+function checkIfBodyAccordionNotExist(header) {
+	var flag = false;
+	if (header.next('.bodyAccordion').length == 0) {
+		flag = true;
+	}
+	return flag;
+}
+
+/**
+ * [createBodyAccordionEventListener function that create a body of the accordion header clicked and append it to accordion]
+ * @param  {[type]} header            [header clicked]
+ * @param  {[Object]} singleWeatherData [single weather object]
+ * @return {[type]}                   [description]
+ */
+function createBodyAccordionEventListener(header, singleWeatherData) {
+	var index = header.index('.headerAccordion');
+	console.log(index);
+	var bodyAccordionDiv = createBodyAccordion(singleWeatherData);
+	$('.accordion')[index].append(bodyAccordionDiv[0]);
 }
 
 /**
@@ -182,10 +238,11 @@ function createBodyAccordion(singleWeatherData) {
 	 * @type {[type]}
 	 */
 	var bodyAccordionDiv = $('<div></div>').addClass('bodyAccordion');
-	bodyAccordionDiv.html("Some text and markup")
-
-	//var prova = document.getElementsByClassName('bodyAccordion');
-
+	var pLocation = $('<p> Località: ' + singleWeatherData.station.city + ' in provincia di ' + singleWeatherData.station.province.name + ', ' + singleWeatherData.station.region.name + ' in ' + singleWeatherData.station.nation.name + '  </p>');
+	var imageWebcam = getWebcamImage(singleWeatherData);
+	var pClimate = $('<p> Clima: ' + singleWeatherData.station.climate + '</p>')
+	var pDescription = $("<p> Descrizione della stazione: " + singleWeatherData.station.description + "</p>");
+	bodyAccordionDiv.append(pLocation, pDescription, pClimate, imageWebcam);
 	return bodyAccordionDiv;
 }
 
@@ -197,13 +254,13 @@ function createBodyAccordion(singleWeatherData) {
 function getFlagImage(singleWeatherData) {
 	switch (singleWeatherData.station.nation.name) {
 		case "Italia":
-			return $('<img></img>').addClass('nationFlag').attr('src', 'media/italy_flag.jpg');
+			return $('<img></img>').addClass('nationFlag').attr({src: 'media/italy_flag.jpg', alt: 'italy'});
 		case "Francia":
-			return $('<img></img>').addClass('nationFlag').attr('src', 'media/france_flag.jpeg');
+			return $('<img></img>').addClass('nationFlag').attr({src: 'media/france_flag.jpeg', alt: 'france'});
 		case "Svizzera":
-			return $('<img></img>').addClass('nationFlag').attr('src', 'media/swiss_flag.jpg');
+			return $('<img></img>').addClass('nationFlag').attr({src: 'media/swiss_flag.jpg', alt: 'swiss'});
 		default:
-			return $('<img></img>').addClass('nationFlag').attr('src', 'media/no_flag_avaible.jpg');
+			return $('<img></img>').addClass('nationFlag').attr({src: 'media/no_flag_avaible.jpeg', alt: 'no image avaible'});
 	}
 }
 
@@ -233,6 +290,31 @@ function checkIfAccordionIsOpen(accordionIndex) {
 		}
 	}
 	return flag;
+}
+
+/**
+ * [getLastUpdate function to print the last date of the page's refresh]
+ * @return {[type]} [description]
+ */
+function getLastUpdate(){
+	var formatDate = new Date();
+	var date = $('p.lastUpdate')
+	date.html(formatDate.toUTCString());
+	var divDate = $('div#divDate');
+	divDate.append(date);
+}
+
+/**
+ * [getWebcamImage  function to get the webcam image]
+ * @param  {[Object]} singleWeatherData [description]
+ * @return {[type]}                   [description]
+ */
+function getWebcamImage(singleWeatherData){
+	if (singleWeatherData.station['webcam'] == ""){
+		return $('<p>').html('there is no image for this weather station').addClass('imageNotFound');
+	}else{
+		return $('<img>').addClass('webcamImg').attr('src', singleWeatherData.station['webcam']);
+	}
 }
 
 //call the function that get the weather data
---
+++ readme - sara.txt
@@ -0,0 +1,4 @@
+Aggiornamenti del sito:
+ - implementata la data dell'ultimo aggiornamento dei dati
+ - aggiunto contenuto al bodyAccordion
+ 
